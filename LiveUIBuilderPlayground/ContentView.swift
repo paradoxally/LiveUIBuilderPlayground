3c10f5e05d10939d74ff5c1c45f0506ef8a2dded
Nino Vitale <nino.vitale@icloud.com>
2020-02-02 16:05:25 +0000

    Added boilerplate files.

---
+++ LiveUIBuilderPlayground/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LiveUIBuilderPlayground
+//
+//  Created by Nino Vitale on 2/2/20.
+//  Copyright © 2020 Nino Vitale. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
