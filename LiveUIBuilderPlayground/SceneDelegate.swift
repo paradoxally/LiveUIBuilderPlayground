2a3890d3b99b50c22c490af16a60b79d3ba5d2c8
Nino Vitale <nino.vitale@icloud.com>
2020-02-02 18:19:51 +0000

    Refactor to scene delegate setup.

--- LiveUIBuilderPlayground/SceneDelegate.swift
+++ LiveUIBuilderPlayground/SceneDelegate.swift
@@ -15,8 +15,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // IF YOU'RE USING UIKIT, UNCOMMENT THIS SEGMENT
         
+        // IF YOU'RE USING UIKIT, COMMENT SWIFTUI OUT AND UNCOMMENT BELOW
+        setupUIKit(with: scene)
+        
+        // IF YOU'RE USING SWIFTUI, COMMENT UIKIT OUT AND UNCOMMENT BELOW
+        //setupSwiftUI(with: scene)
+    }
+
+    func setupUIKit(with scene: UIScene) {
         // Create the UIKit view that provides the window contents.
         let viewController = ViewController()
 
@@ -27,10 +34,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             self.window = window
             window.makeKeyAndVisible()
         }
-        
-        // IF YOU'RE USING SWIFTUI, COMMENT THE UIKIT SECTION OUT AND UNCOMMENT BELOW
-        
-        /*
+    }
+    
+    func setupSwiftUI(with scene: UIScene) {
         // Create the SwiftUI view that provides the window contents.
         let contentView = ContentView()
 
@@ -40,37 +46,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             window.rootViewController = UIHostingController(rootView: contentView)
             self.window = window
             window.makeKeyAndVisible()
-        }*/
+        }
     }
-
-    func sceneDidDisconnect(_ scene: UIScene) {
-        // Called as the scene is being released by the system.
-        // This occurs shortly after the scene enters the background, or when its session is discarded.
-        // Release any resources associated with this scene that can be re-created the next time the scene connects.
-        // The scene may re-connect later, as its session was not neccessarily discarded (see `application:didDiscardSceneSessions` instead).
-    }
-
-    func sceneDidBecomeActive(_ scene: UIScene) {
-        // Called when the scene has moved from an inactive state to an active state.
-        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
-    }
-
-    func sceneWillResignActive(_ scene: UIScene) {
-        // Called when the scene will move from an active state to an inactive state.
-        // This may occur due to temporary interruptions (ex. an incoming phone call).
-    }
-
-    func sceneWillEnterForeground(_ scene: UIScene) {
-        // Called as the scene transitions from the background to the foreground.
-        // Use this method to undo the changes made on entering the background.
-    }
-
-    func sceneDidEnterBackground(_ scene: UIScene) {
-        // Called as the scene transitions from the foreground to the background.
-        // Use this method to save data, release shared resources, and store enough scene-specific state information
-        // to restore the scene back to its current state.
-    }
-
-
 }
 
